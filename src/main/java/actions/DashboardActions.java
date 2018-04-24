6d769b7f9d53f7ac62b43edd10ecb361de3ece62
Timoshpolskii <timoooha123@gmail.com>
2018-04-24 13:06:52 +0300

    added dashboard tests

--- src/main/java/actions/DashboardActions.java
+++ src/main/java/actions/DashboardActions.java
@@ -1,18 +1,21 @@
 package actions;
 
-import pages.AddFilmPage;
 import pages.DashboardPage;
 import utils.HasWaiter;
 
 public class DashboardActions implements HasWaiter {
     private DashboardPage dashboardPage = new DashboardPage();
-    private AddFilmPage addFilmPage = new AddFilmPage();
 
+    @Override
     public boolean waitForPageToBeLoaded() {
         return waiter(15).waitDisplayed(dashboardPage.btnSettings);
     }
 
     public void pressAddButton() {
         dashboardPage.btnAdd.click();
+    }
+
+    public boolean isSettingsButtonDisplayed() {
+        return dashboardPage.btnSettings.isDisplayed();
     }
 }
--- src/test/java/tests/DashBoardTests.java
+++ src/test/java/tests/DashBoardTests.java
@@ -1,40 +1,59 @@
 package tests;
 
-import actions.AddFilmActions;
-import actions.DashboardActions;
-import actions.LoginActions;
 import driver.SeleniumDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import steps.AddFilmSteps;
+import steps.DashBoardSteps;
+import steps.FilmDetailsSteps;
+import steps.LoginSteps;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 public class DashBoardTests extends BaseTest {
 
-    private DashboardActions dashboardActions = new DashboardActions();
-    private AddFilmActions addFilmActions = new AddFilmActions();
-    private LoginActions loginActions = new LoginActions();
+    private LoginSteps loginSteps = new LoginSteps();
+    private DashBoardSteps dashBoardSteps = new DashBoardSteps();
+    private AddFilmSteps addFilmSteps = new AddFilmSteps();
+    private FilmDetailsSteps filmDetailsSteps = new FilmDetailsSteps();
 
     @BeforeClass
     public void loginWithValidCredentials() {
         //TODO refactor this part
         SeleniumDriver.getDriver().get("http://localhost/php4dvd/");
-        loginActions.enterUserName("admin");
-        loginActions.enterPassword("admin");
-        loginActions.tapSubmitButton();
-        dashboardActions.waitForPageToBeLoaded();
+        loginSteps.loginWithValidCredentials("admin", "admin");
     }
 
     @Test
-    public void checkNameOfNewAddedFilm() {
+    public void checkInfoOfNewAddedFilm() {
         //TODO save film info in FILM object
-        String filmTitle = "New Film Title";
-        String filmYear = "1990";
+        String expectedFilmName = "New Film Title";
+        String expectedFilmYear = "1990";
+        String expectedFilmLanguage = "English";
+        String expectedFilmPersonalNotes = "Film personal notes";
 
-        dashboardActions.pressAddButton();
-        addFilmActions.waitForPageToBeLoaded();
-        addFilmActions.addTitleOfFilm(filmTitle);
-        addFilmActions.addYearOfFilm(filmYear);
-        addFilmActions.saveFilm();
+        dashBoardSteps.pressAddButton();
+        addFilmSteps.addTitleOfFilm(expectedFilmName);
+        addFilmSteps.addYearOfFilm(expectedFilmYear);
+        addFilmSteps.addPersonalNotesOfFilm(expectedFilmPersonalNotes);
+        addFilmSteps.addLanguageOfFilm(expectedFilmLanguage);
+        addFilmSteps.saveFilm();
+
+        //TODO create assertion for multiple values
+        String expectedFilmTitle = expectedFilmName + " (" + expectedFilmYear + ")";
+        String actualFilmTitle = filmDetailsSteps.getFilmTitle();
+        assertThat("Actual name of film should be matched with expected",
+                actualFilmTitle, equalTo(expectedFilmTitle));
+
+        String actualFilmLanguage = filmDetailsSteps.getFilmLanguage();
+        assertThat("Actual language of film should be matched with expected",
+                actualFilmLanguage, equalTo("Languages: " + expectedFilmLanguage));
+
+        String actualFilmPersonalNotes = filmDetailsSteps.getFilmPersonalNotes();
+        assertThat("Actual personal notes of film should be matched with expected",
+                actualFilmPersonalNotes, equalTo(expectedFilmPersonalNotes));
     }
 
     @AfterClass
