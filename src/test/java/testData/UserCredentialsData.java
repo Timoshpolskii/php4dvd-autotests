9b59cfbb69bada6d646a1540edd032fb1b23d7c4
Timoshpolskii <timoooha123@gmail.com>
2018-05-11 15:19:43 +0300

    filed paths to files

--- src/test/java/testData/UserCredentialsData.java
+++ src/test/java/testData/UserCredentialsData.java
@@ -10,7 +10,7 @@ public class UserCredentialsData {
 
     @DataProvider(name = "admin_user")
     public Object[][] adminData() throws FileNotFoundException {
-        String ADMIN_USER_FILEPATH = "/Users/andrewtimoshpolskiy/Documents/workspace/php4dvd-autotests/src/test/resources/fixtures/admin_user_credentials.json";
+        String ADMIN_USER_FILEPATH = "../../src/test/resources/fixtures/admin_user_credentials.json";
         TestDataReader<UserObject> userCredentials = new TestDataReader(ADMIN_USER_FILEPATH, UserObject.class);
         UserObject user = userCredentials.read();
         return new Object[][]{{user.getUsername(), user.getPassword()}};
@@ -18,7 +18,7 @@ public class UserCredentialsData {
 
     @DataProvider(name = "guest_user")
     public Object[][]  guestData() throws FileNotFoundException {
-        String GUEST_USER_FILEPATH = "/Users/andrewtimoshpolskiy/Documents/workspace/php4dvd-autotests/src/test/resources/fixtures/guest_user_credentials.json";
+        String GUEST_USER_FILEPATH = "../../src/test/resources/fixtures/guest_user_credentials.json";
         TestDataReader<UserObject> userCredentials = new TestDataReader(GUEST_USER_FILEPATH, UserObject.class);
         UserObject user = userCredentials.read();
         return new Object[][]{{user.getUsername(), user.getPassword()}};
