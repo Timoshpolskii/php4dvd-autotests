16ec290eeaa050a9e3e37812926efd1acf828412
Timoshpolskii <timoooha123@gmail.com>
2018-05-11 13:39:56 +0300

    -added refactoring -added session cookie in @beforeMethod

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,14 +1,27 @@
 package tests;
 
 import driver.SeleniumDriver;
+import org.openqa.selenium.Cookie;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import steps.LoginSteps;
 
 public class BaseTest {
-    @BeforeMethod
+    private LoginSteps loginSteps = new LoginSteps();
+    private static Cookie currentSession;
+
+    @BeforeSuite()
     public void setUp() {
         SeleniumDriver.getDriver().get("http://localhost/php4dvd/");
+        loginSteps.loginWithValidCredentials("admin", "admin");
+        currentSession = SeleniumDriver.getDriver().manage().getCookieNamed("PHPSESSID");
+    }
+
+    @BeforeMethod()
+    public void setSession() {
+        SeleniumDriver.getDriver().manage().addCookie(currentSession);
     }
 
     @AfterMethod
