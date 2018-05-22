d13c23ea06e0590d6b16bed6588f350bc33e75e7
Timoshpolskii <timoooha123@gmail.com>
2018-05-22 15:33:05 +0300

    -refactored waiter -added custom expected conditions

---
+++ src/main/java/driver/CustomExpectedConditions.java
@@ -0,0 +1,34 @@
+package driver;
+
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+
+class CustomExpectedConditions {
+
+    static ExpectedCondition<Boolean> elementToBeAbsent(WebElement element) {
+        return driver -> {
+            boolean result;
+            try {
+                result = !(element.isDisplayed());
+            } catch (NoSuchElementException | StaleElementReferenceException | TimeoutException e) {
+                result = true;
+            }
+            return result;
+        };
+    }
+
+    static ExpectedCondition<Boolean> elementToBeDisplayed(WebElement element) {
+        return driver -> {
+            boolean result;
+            try {
+                result = (element.isDisplayed());
+            } catch (NoSuchElementException | StaleElementReferenceException | TimeoutException e) {
+                result = false;
+            }
+            return result;
+        };
+    }
+}
