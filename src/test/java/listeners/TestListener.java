e05ca46ce7aa117912000d56483599d648984d01
Timoshpolskii <timoooha123@gmail.com>
2018-07-29 14:33:37 +0300

    added screenshots on fail

---
+++ src/test/java/listeners/TestListener.java
@@ -0,0 +1,33 @@
+package listeners;
+
+import helper.Screenshot;
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+
+public class TestListener extends TestListenerAdapter {
+
+    @Override
+    public void onTestFailure(ITestResult tr) {
+        //TODO implement logging
+        super.onTestFailure(tr);
+        Screenshot.takeScreenshot(tr.getName());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult tr) {
+        //TODO implement logging
+        super.onTestSkipped(tr);
+        Screenshot.takeScreenshot(tr.getName());
+    }
+
+    private String getTestName(ITestResult tr) {
+        return tr.getTestClass().getRealClass().getSimpleName() + "." + tr.getTestName();
+    }
+}
+
+//    @Override
+//    public void onStart(ITestContext testContext) {
+//        super.onStart(testContext);
+//        Screenshot.takeScreenshot(testContext.getName());
+//    }
+//}
