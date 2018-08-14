cdae1a32852293c146efa519658624168cffde6e
Timoshpolskii <timoooha123@gmail.com>
2018-08-14 16:07:46 +0300

    -added cross browser support[Firefox only support]

--- src/main/java/driver/DriverProvider.java
+++ src/main/java/driver/DriverProvider.java
@@ -1,9 +1,12 @@
 package driver;
 
+import helper.PropertiesReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,15 +18,32 @@ public class DriverProvider {
     private DriverProvider(){}
 
     public static synchronized WebDriver getDriver() {
-        //TODO create capability for different browsers
         if (driver == null) {
-            String projectPath = System.getProperty("user.dir");
-            System.setProperty("webdriver.chrome.driver", projectPath + "/src/main/resources/chromedriver");
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            setDefaultImplicitlyWait();
-            LogManager.getLogger().info("SUCCESS create WebDriver");
+            return initDriver();
         }
+        else return driver;
+    }
+
+    private static WebDriver initDriver() {
+        String browser = getBrowserName();
+        String projectPath = System.getProperty("user.dir");
+        switch (browser) {
+            case "Firefox" :
+                System.setProperty("webdriver.gecko.driver", projectPath + "/src/main/resources/geckodriver");
+                driver = new FirefoxDriver();
+                break;
+            case "Safari" : //NOTE: Safari is not supported due to numerous server-side errors in driver
+                driver = new SafariDriver();
+                break;
+            case "Chrome" :
+                System.setProperty("webdriver.chrome.driver", projectPath + "/src/main/resources/chromedriver");
+                driver = new ChromeDriver();
+                break;
+            default : throw new RuntimeException("BROWSER NAME is incorrect or not provided");
+        }
+        driver.manage().window().maximize();
+        setDefaultImplicitlyWait();
+        LogManager.getLogger().info("SUCCESS create WebDriver for browser " + browser);
         return driver;
     }
 
@@ -37,6 +57,21 @@ public class DriverProvider {
         if (driver != null) {
             driver.manage().timeouts().implicitlyWait(DEFAULT_IMPLICITLY_WAIT, TimeUnit.SECONDS);
         }
+    }
+
+    private static String readBrowserFromPropertiesFile() {
+        String filePath = "/src/test/resources/browser_names.properties";
+        return PropertiesReader.readPropertyFromFile(filePath, "browser_name");
+    }
+
+    private static String getBrowserName() {
+        String browser = System.getProperty("browser");
+        if (browser == null) {
+            log.info("BROWSER is not set from gradle parameters. Will read browser from properties file");
+            browser = readBrowserFromPropertiesFile();
+        }
+        log.info("CURRENT browser is " + browser);
+        return browser;
     }
 
     public static void quitDriver() {
